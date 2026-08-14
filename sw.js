3678dce3f60558207752e4d9a5643e667ef1cd91
YSamiii <samanthayaosy@gmail.com>
2026-08-14 14:07:29 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="xixi-fox-week-v8";
-const ASSETS=['./','./index.html?v=6','./manifest.webmanifest','./fox-icon-v6-192.png','./fox-icon-v6-512.png','./fox-touch-icon-v6.png','./fox-app-icon.png','./top-avatar.png','./icon-lang.svg','./icon-math.svg','./icon-eng.svg','./icon-rhyme.svg','./icon-music.svg','./icon-lesson.svg','./icon-ext.svg','./icon-other.svg'];
-self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request)));});
+const CACHE="xixi-fox-week-v9";
+const ASSETS=['./','./index.html?v=9','./manifest.webmanifest?v=9','./icon-192.png?v=9','./icon-512.png?v=9','./apple-touch-icon.png?v=9','./apple-touch-icon-precomposed.png?v=9','./favicon.png?v=9','./fox-app-icon.png','./top-avatar.png','./icon-lang.svg','./icon-math.svg','./icon-eng.svg','./icon-rhyme.svg','./icon-music.svg','./icon-lesson.svg','./icon-ext.svg','./icon-other.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
