dd921daf23f4b09d47c7772a8b58cd0adf5c0095
YSamiii <samanthayaosy@gmail.com>
2026-08-22 13:28:38 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,27 @@
-const CACHE="xixi-ece-v20260818-2";
-const SHELL=["./","./index.html","./manifest.webmanifest","./apple-touch-icon.png","./icon-192.png","./icon-512.png","./top-avatar.png","./fox-app-icon.png","./triangle-reference.png","./icon-lang.svg","./icon-math.svg","./icon-eng.svg","./icon-lesson.svg","./icon-other.svg"];
+const CACHE="xixi-ece-v20260822-3";
+const SHELL=["./","./index.html","./manifest.webmanifest","./apple-touch-icon.png","./icon-192.png","./icon-512.png","./top-avatar.png","./fox-app-icon.png","./triangle-reference.png","./icon-lang.svg","./icon-math.svg","./icon-eng.svg","./icon-lesson.svg","./icon-other.svg","./data/curriculum.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.pathname.endsWith("/data/curriculum.json")){e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match(e.request)));return;}if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  const u=new URL(e.request.url);
+  if(u.pathname.endsWith("/data/curriculum.json")){
+    e.respondWith(
+      fetch(e.request,{cache:"no-store"}).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy));
+        return res;
+      }).catch(()=>caches.match(e.request))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      if(res && res.ok){
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy));
+      }
+      return res;
+    }))
+  );
+});
